08f1c39ba9a988c1697084f94fd5dc048f19407e
Ivan Golubey <ig2108@yahoo.com>
2019-09-10 00:59:30 +0300

    complete hw-01

--- hw-01/js/task-06.js
+++ hw-01/js/task-06.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let total = 0;
+let userInput;
+let userInputNumber;
+
+do {
+  userInput = prompt('Введите число!');
+
+  userInputNumber = Number(userInput);
+
+  const userInputIsNan = Number(isNaN(userInputNumber));
+
+  if (userInputIsNan) {
+    alert('Было введено не число, попробуйте еще раз');
+
+    userInputNumber = 0;
+    total += userInputNumber;
+  } else {
+    total += userInputNumber;
+  }
+} while (userInput !== null);
+
+alert(`Общая сумма чисел равна ${total}`);
