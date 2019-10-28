41315f838b4ef80ed72944418ce0e1d616364b6e
Ivan Golubey <ig2108@yahoo.com>
2019-10-28 21:59:10 +0200

    js complete hw-06

--- hw-06/js/task-01-10.js
+++ hw-06/js/task-01-10.js
@@ -77,9 +77,22 @@ console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 //======================== TASK 10 ========================
-// Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
-const getSortedUniqueSkills = users => users.filter(user => user.skills);
+const getSortedUniqueSkills = users =>
+  users
+    .reduce((allSkills, user) => {
+      allSkills.push(...user.skills);
+
+      return allSkills;
+    }, [])
+    .reduce((uniqueSkills, skill) => {
+      if (!uniqueSkills.includes(skill)) {
+        uniqueSkills.push(skill);
+      }
+
+      return uniqueSkills;
+    }, [])
+    .sort();
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
